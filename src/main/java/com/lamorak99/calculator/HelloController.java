2e6c65a29f830bf38176a0a8b4ab605ea1f91a20
lamorak99 <lamorak@hushmail.com>
2017-11-23 16:36:24 -0500

    Added HelloController

---
+++ src/main/java/com/lamorak99/calculator/HelloController.java
@@ -0,0 +1,15 @@
+package com.lamorak99.calculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam String name) {
+		return "Hello "+name;
+	}
+
+}
